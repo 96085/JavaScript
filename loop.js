033566339a7d0770196a38a258d5e14b456df962
Aquib Rabbani <95527271+96085@users.noreply.github.com>
2024-07-03 11:00:03 +0530

    Update loop.js

--- loop.js
+++ loop.js
@@ -1,2 +1,25 @@
 ****************************************************************************************************************************************************************************************************************************
-                                                                                                  Loops
+  Loops
+ for loop
+ for(let i=0; i<n; i++){
+     if(statement){
+         body
+     }
+     else{
+         body
+     }
+ }
+
+ while loop
+ initialization
+ condition{
+     body
+     increment
+ }
+
+ do while loop
+ do statment{
+    Body
+     increment
+ }
+ condition
