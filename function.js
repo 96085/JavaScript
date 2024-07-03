432cb8216814f23794c43c6cd2b9af2f58bdeb6f
Aquib Rabbani <95527271+96085@users.noreply.github.com>
2024-07-03 20:59:35 +0530

    function.js

---
+++ function.js
@@ -0,0 +1,37 @@
+A function definition (also called a function declaration, or function statement) consists of the function keyword, followed by:
+
+The name of the function.
+A list of parameters to the function, enclosed in parentheses and separated by commas.
+The JavaScript statements that define the function, enclosed in curly braces, { /* … */ }.
+For example, the following code defines a simple function named square:
+
+function square(number) {
+  return number * number;
+}
+square (); //function call
+
+// The following variables are defined in the global scope
+const num1 = 20;
+const num2 = 3;
+const name = "Chamakh";
+
+// This function is defined in the global scope
+function multiply() {
+  return num1 * num2;
+}
+
+console.log(multiply()); // 60
+
+// A nested function example
+function getScore() {
+  const num1 = 2;
+  const num2 = 3;
+
+  function add() {
+    return `${name} scored ${num1 + num2}`;
+  }
+
+  return add();
+}
+
+console.log(getScore()); // "Chamakh scored 5"
